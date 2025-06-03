3eefa2ac3c7ffe4c00e8169c501002f7956556ac
Vinayak Pandey <vinayak912002@gmail.com>
2025-06-03 21:08:52 +0530

    added leetcode problem 746

--- Leetcode/Codes/Sol746.java
+++ Leetcode/Codes/Sol746.java
@@ -4,15 +4,33 @@ import java.util.*;
 public class Sol746 {
     public static void main(String[] args){
         int[] cost = {10, 15, 20};
-        System.out.println(minCostClimbingStairs(cost)); // Output: 15
+        System.out.println(minCostClimbingStairsMemo(cost)); // Output: 15
     }
 
+    // recursive solution
     public static int minCostClimbingStairs(int[] cost){
         return Math.min(dfs(cost, 0), dfs(cost, 1));
     }
-    // recursive solution
+
     public static int dfs(int[] cost, int i){
         if (i>=cost.length) return 0;
         return cost[i] + Math.min(dfs(cost, i+1), dfs(cost, i+2));
+    }
+
+    // memoization solution
+    private static HashMap<Integer, Integer> memo;
+    public static int minCostClimbingStairsMemo(int[] cost){
+        memo = new HashMap<>();
+        return Math.min(dfs(cost, 0), dfsMemo(cost, 1));
+    }
+
+    
+    private static int dfsMemo(int[] cost, int i){
+        if (i >=cost.length) return 0;
+        if (memo.containsKey(i)) return memo.get(i);
+        
+        int result = cost[i] + Math.min(dfsMemo(cost, i+1), dfsMemo(cost, i+2));
+        memo.put(i, result);
+        return result;
     }
 }
